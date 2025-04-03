4a2c1c501343277801ff45398d2fe3148174a392
dorelly <dorelly.rosario@crisanto.pe>
2025-04-03 12:36:41 -0500

    scroll in component home solve

--- app/components/organisms/HomeSolve.tsx
+++ app/components/organisms/HomeSolve.tsx
@@ -17,7 +17,7 @@ const HomeSolve = ({ data }: HomeBannerProps) => {
 
   // Ícono de Igual
   const EqualIcon = () => (
-    <div className="bg-primary p-2 rounded-full shadow-md mx-2 flex items-center justify-center">
+    <div className="container__icon">
       <svg className="h-5 w-5 text-white" viewBox="0 0 24 24" fill="currentColor">
         <rect x="4" y="10" width="16" height="2" />
         <rect x="4" y="14" width="16" height="2" />
@@ -31,27 +31,27 @@ const HomeSolve = ({ data }: HomeBannerProps) => {
       {/* Título */}
       <p className="title__home-problems-solve">{data.title}</p>
 
-
+      <div className="cards__home-problems-solve">
       {data.card.map((item: CardItem, index: number) => (
-      <div
-          key={index}
-          className="card__home-problems-solve"
-        >
-          {/* Contenedor en fila para los dos span */}
-          <div className="container__title-card">
-            <span className="title__card-home-solve">{item.title}</span>
-            {item.text && <EqualIcon />}
-            <span className="text__card-home-solve">{item.text}</span>
-          </div>
+          <div
+              key={index}
+              className="card__home-problems-solve"
+            >
+              {/* Contenedor en fila para los dos span */}
+              <div className="container__title-card">
+                <span className="title__card-home-solve">{item.title}</span>
+                {item.text && <EqualIcon />}
+                <span className="text__card-home-solve">{item.text}</span>
+              </div>
 
-          {/* Línea divisoria */}
-          <hr className="line" />
+              {/* Línea divisoria */}
+          {/*     <hr className="line" /> */}
 
-          {/* Descripción */}
-          <p className="">{item.description}</p>
+              {/* Descripción */}
+              <p className="description__card-home-solve">{item.description}</p>
+            </div>
+          ))}
         </div>
-      ))}
-
     </section>
   );
 };
