4eb23aef2e72437e61b7182a96b5175b2b30f33b
ricardo leonardo olivares ventura <rlov@MacBook-Air-de-ricardo.local>
2025-04-06 19:32:30 -0500

    Get impactourservices data

--- app/components/organisms/ImpactOurServices.tsx
+++ app/components/organisms/ImpactOurServices.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useGenerals } from "@/app/context/generals.context";
 import Image from "next/image";
 import React from "react";
 import img1 from "../../../public/images/impactOurServices/1.png";
@@ -7,9 +10,14 @@ import img4 from "../../../public/images/impactOurServices/4.png";
 import coverImage from "../../../public/images/impactOurServices/background.jpeg";
 
 export default function ImpactOurServices() {
+  const { home } = useGenerals();
+  const ourServicesImpact = home?.ourServicesImpact;
+  const options = ourServicesImpact?.cards ?? [];
   //Campo image, será la imagen de fondo
 
-  const options = [
+  console.log({ ourServicesImpact });
+
+  /*   const options = [
     {
       title: "Boost Employee Productivity",
       img: img1,
@@ -26,7 +34,7 @@ export default function ImpactOurServices() {
       title: "Eco-Friendly Solutions",
       img: img4,
     },
-  ];
+  ]; */
 
   return (
     <div className="relative mt-20 pb-16 pt-16 max-[900px]:mt-10 max-[900px]:pt-12">
@@ -41,10 +49,10 @@ export default function ImpactOurServices() {
       <div className="absolute inset-0 z-[-1] bg-primary opacity-[.85]"></div>
       <div className="bg-red z-[10]">
         <p className="mb-12 text-center text-3xl font-semibold text-white max-[900px]:mb-8 max-[900px]:text-xl">
-          The Impact Of Our Services
+          {ourServicesImpact?.title ?? ""}
         </p>
         <div className="grid grid-cols-4 gap-16 px-28 max-xl:gap-6 max-xl:px-16 max-[1100px]:px-10 max-[900px]:px-6 max-[890px]:grid-cols-2">
-          {options.map((option, index) => (
+          {options.map((option: any, index: number) => (
             <div
               key={index}
               className="flex flex-col gap-3 border border-solid border-[white] px-6 py-8 max-[900px]:px-4 max-[900px]:py-6"
@@ -53,7 +61,7 @@ export default function ImpactOurServices() {
                 <div className="flex h-[60px] w-[60px] items-center justify-center max-[900px]:h-[40px] max-[900px]:w-[40px]">
                   <Image
                     src={option.img}
-                    alt=""
+                    alt={option.title}
                     style={{
                       objectFit: "contain",
                       height: "100%",
